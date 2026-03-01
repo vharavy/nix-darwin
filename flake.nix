c2c41f3d9ebfbcbe8fd21a2ae877f36b364d5399
Vitali Haravy <humaneprogrammer@gmail.com>
2026-03-01 02:55:24 +0000

    Remove unused Nerd Fonts

--- flake.nix
+++ flake.nix
@@ -56,8 +56,6 @@
           ];
 
           fonts.packages = [
-            pkgs.nerd-fonts.zed-mono
-            pkgs.nerd-fonts.envy-code-r
             pkgs.nerd-fonts.jetbrains-mono
           ];
 
